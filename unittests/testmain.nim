ee833a5b308924c456008d8f209010e6112a3389
Arne Döring <arne.doering@gmx.net>
2017-08-30 20:12:58 +0200

    unittest

---
+++ unittests/testmain.nim
@@ -0,0 +1,24 @@
+include ../fancygl
+
+proc main() =
+  type TestType = object
+    a,b,c,d: float32
+
+  var ab = ArrayBuffer[TestType](handle : 7)
+  var v: ArrayBufferView[TestType, float32]
+  v.buffer = ab
+  v.stride = 16
+  v.absoluteoffset = 0
+
+  v.relativeoffset = 0
+  assert ab.view(a) == v
+  v.relativeoffset = 4
+  assert ab.view(b) == v
+  v.relativeoffset = 8
+  assert ab.view(c) == v
+  v.relativeoffset = 12
+  assert ab.view(d) == v
+
+
+when isMainModule:
+  main()
