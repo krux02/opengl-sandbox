19194dea37e010af636a20511934af6e1c38b8d9
Arne Döring <arne.doering@gmx.net>
2017-09-07 14:20:50 +0200

    foobar

--- examples/kdtree.nim
+++ examples/kdtree.nim
@@ -1,4 +1,5 @@
 import glm
+import ../fancygl
 
 type
   KdTree = object
@@ -6,49 +7,63 @@ type
     rootIdx: int
 
   KdNode = object
-    x: Vec3f
+    pos: Vec3f
     left,right: int32
 
   Box = object
     min,max: Vec3f
 
-proc split(box: Box; value: float32; dim: int): tuple[l,r: Box] =
-  assert( box.min[dim] <= value )
-  assert( value <= box.max[dim] )
+  DimIndex = 0..2
 
-  var l = box
-  var r = box
+proc split(box: Box; value: float32; dim: DimIndex): tuple[left,right: Box] =
+  if value < box.min[dim] or box.max[dim] < value:
+    echo s"illigal split ${box.min[dim]} < $value < ${box.max[dim]}"
 
-  l.max[dim] = value
-  r.min[dim] = value
+  result.left  = box
+  result.right = box
+
+  result.left.max[dim] = value
+  result.right.min[dim] = value
 
 
-proc leafNodeBoxes(this: KdTree, node: KdNode, boundingBox: Box, dim: int; dest: var seq[Box]): void =
-  let (leftBox,rightBox) = boundingBox.split(node.x[dim], dim)
+proc leafNodeBoxes(this: KdTree, node: KdNode, boundingBox: Box, dim: DimIndex; depth: int; maxDepth: int, dest: var seq[Box]): void =
+
+  if depth == maxDepth:
+    dest.add boundingBox
+    return
+
+  let value = node.pos[dim]
+  if value < boundingBox.min[dim] or boundingBox.max[dim] < value:
+    echo s"illigal split ${boundingBox.min[dim]} < $value < ${boundingBox.max[dim]}"
+    dest.add boundingBox
+    return
+
+  let (leftBox,rightBox) = boundingBox.split(value, dim)
   if node.left >= 0:
-    leafNodeBoxes(this, this.data[node.left], leftBox, (dim + 1) mod 3, dest):
+    leafNodeBoxes(this, this.data[node.left], leftBox, (dim + 1) mod 3, depth + 1, maxDepth, dest):
   else:
     dest.add leftBox
 
   if node.right >= 0:
-    leafNodeBoxes(this, this.data[node.right], rightBox, (dim + 1) mod 3, dest):
+    leafNodeBoxes(this, this.data[node.right], rightBox, (dim + 1) mod 3, depth + 1, maxDepth, dest):
   else:
     dest.add rightBox
 
-proc leafNodeBoxes(this: KdTree): seq[Box] =
+proc leafNodeBoxes(this: KdTree; maxDepth: int = -1): seq[Box] =
+  let maxDepth = if maxDepth < 0: high(int) else: maxDepth
   var boundingBox = Box(min: vec3f(Inf), max: vec3f(-Inf))
   for node in this.data:
-    boundingBox.min = min(node.x, boundingBox.min)
-    boundingBox.max = max(node.x, boundingBox.max)
+    boundingBox.min = min(boundingBox.min, node.pos)
+    boundingBox.max = max(boundingBox.max, node.pos)
 
   result.newSeq(0)
-  leafNodeBoxes(this, this.data[this.rootIdx], boundingBox, 0, result)
+  leafNodeBoxes(this, this.data[this.rootIdx], boundingBox, 0, 0, maxDepth, result)
 
 
-proc divide_by_mean(t: var KdTree, a,b: int, dim: int): int =
+proc divide_by_mean(t: var KdTree, a,b: int, dim: DimIndex): int =
   var mean: float32
   for i in a ..< b:
-    mean += t.data[i].x.arr[dim]
+    mean += t.data[i].pos.arr[dim]
   mean /= float32(b - a)
 
   var a = a
@@ -56,36 +71,35 @@ proc divide_by_mean(t: var KdTree, a,b: int, dim: int): int =
 
   while a < b:
     swap(t.data[a], t.data[b])
-    while t.data[a].x.arr[dim] < mean:
+    while t.data[a].pos.arr[dim] < mean:
       a += 1
-    while t.data[b].x.arr[dim] >= mean:
+    while t.data[b].pos.arr[dim] >= mean:
       b -= 1
 
   return a
 
-proc subtree_sort(t: var KdTree; a,b: int, i: int): int =
+proc subtree_sort(t: var KdTree; a,b: int, dim: DimIndex): int =
   if a == b:
     return -1
 
 
-  result = t.divide_by_mean(a, b, i)
+  result = t.divide_by_mean(a, b, dim)
 
   if 0 <= result:
-    t.data[result].left  = subtree_sort(t, a,   result, (i + 1) mod 3).int32
-    t.data[result].right = subtree_sort(t, result+1, b, (i + 1) mod 3).int32
+    t.data[result].left  = subtree_sort(t, a,   result, (dim + 1) mod 3).int32
+    t.data[result].right = subtree_sort(t, result+1, b, (dim + 1) mod 3).int32
 
 proc init(t: var KdTree): void =
   t.rootIdx = t.subtree_sort(0, t.data.len, 0)
 
-proc nearestIntern(this: KdTree; node_idx: int; pos: Vec4f, dim: int; best_idx: var int, best_dist: var float32, visited: var int): void =
+proc nearestIntern(this: KdTree; node_idx: int; pos: Vec3f, dim: int; best_idx: var int, best_dist: var float32, visited: var int): void =
   if node_idx == -1:
     return
 
   template node: KdNode = this.data[node_idx]
 
-  let d = length(root.x - node.x)
-  let dx = root.x.arr[dim] - node.x[dim]
-  let dx2 = dx * dx
+  let d = length(node.pos - pos)
+  let dx = node.pos.arr[dim] - pos[dim]
 
   visited += 1
 
@@ -98,122 +112,81 @@ proc nearestIntern(this: KdTree; node_idx: int; pos: Vec4f, dim: int; best_idx: 
 
   let dim2 = (dim + 1) mod 3
 
-  this.nearestIntern(if 0 < dx: root.left else: root.right, pos, dim2, best_idx, best_dist, visited)
-  if (dx2 >= best_dist):
+  let first  = if 0 < dx: node.left else: node.right
+  let second = if 0 < dx: node.right else: node.left
+
+  this.nearestIntern(first,  pos, dim2, best_idx, best_dist, visited)
+  if (dx * dx >= best_dist):
     return
-  this.nearestIntern(if 0 < dx: root.right else: root.left, pos, dim2, best_idx, best_dist, visited)
+  this.nearestIntern(second, pos, dim2, best_idx, best_dist, visited)
 
 
 proc nearest(this: KdTree, node: KdNode): tuple[best_idx: int, best_dist: float32, visited: int] =
   result.best_idx = -1
   this.nearestIntern(this.rootIdx, node.pos, 0, result.best_idx, result.best_dist, result.visited)
 
-proc nearestLinear(this: KdTree, node: KdNode): tuple[best_idx: int, best_dist: float32] =
+proc nearestLinear(this: KdTree, pos: Vec3f): tuple[best_idx: int, best_dist: float32] =
 
   result.best_dist = Inf
   for i, node in this.data:
-    let dist = length(node.x - node.x)
+    let dist = length(node.pos - pos)
     if dist < result.best_dist:
       result.best_idx = i
       result.best_dist = dist
 
-const N = 100
-
 import random
 proc rand1(): float32 =
-  float32(random.random(1.0f))
+  float32(random.random(2.0f) - 1.0f)
 
-proc rand_node(): KdNode {.inline.} =
-  result.x.arr = [rand1(), rand1(), rand1()]
+proc rand_pos(): Vec3f = Vec3f(arr: [rand1(), rand1(), rand1()])
 
-proc main(): void =
+proc rand_node(): KdNode = KdNode(pos: rand_pos())
 
-
-  block:
-
-    var wp = KdTree(data: @[
-      KdNode(x: vec3f(2,3,1)),
-      KdNode(x: vec3f(5,4,2)),
-      KdNode(x: vec3f(9,6,3)),
-      KdNode(x: vec3f(4,7,4)),
-      KdNode(x: vec3f(8,1,5)),
-      KdNode(x: vec3f(7,2,6))
-    ])
-    wp.init
-    var testNode = KdNode(x: vec3f(2,3,0))
-    let (found_idx, best_dist, visited) = wp.nearest(testNode);
-    let found = wp.data[found_idx]
-
-    echo ">> WP tree\nsearching for ", testNode.x
-    echo "found ", found.x, " dist ", sqrt(best_dist)
-    echo "seen ", visited, " nodes\n"
-
-  var million = KdTree(data: newSeq[KdNode](N))
+proc rand_tree(N: int): KdTree =
+  result.data = newSeq[KdNode](N)
   for i in 0 ..< N:
-    million.data[i] = rand_node()
-  million.init
+    result.data[i] = rand_node()
+  result.init
 
-  block:
+proc testKdTree(): void =
+  echo "test KdTree"
+  let million = rand_tree(100)
+
+  var sum: int = 0
+  let test_runs = 1000
+  var error_count = 0
+  for i in 1 .. test_runs: # number of test runs
     var testNode = rand_node()
-    let (found_idx, best_dist, visited) = million.nearest(testNode);
-    let found = million.data[found_idx]
+    let (foundA, distA, visited) = million.nearest(testNode)
+    let (foundB, distB) = million.nearestLinear(testNode.pos)
+    if foundA != foundB:
+      echo s"$foundA should be $foundB"
+      echo s"found dist: $distA should be $distB"
+      error_count += 1
+    sum += visited
+  echo s"$error_count errors of $test_runs runs"
+  echo s"visited $sum nodes for $test_runs random findings (${sum/test_runs} per lookup)"
 
-    echo ">> Million tree"
-    echo "searching for ", testNode.x
-    echo "found ", found.x, " dist ", sqrt(best_dist)
-    echo "seen ", visited
+testKdTree()
 
-
-  block:
-    #[ search many random points in million tree to see average behavior
-       tree size vs avg nodes visited:
-       10    ~  7
-       100   ~ 16.5
-       1000    ~ 25.5
-       10000     ~ 32.8
-       100000    ~ 38.3
-       1000000   ~ 42.6
-       10000000  ~ 46.7        ]#
-
-    var sum: int = 0
-    let test_runs = 1000
-    for i in 1 .. test_runs: # number of test runs
-      var testNode = rand_node()
-      let (foundA, distA, visited) = million.nearest(testNode)
-      let (foundB, distB) = million.nearestLinear(testNode)
-      doAssert(foundA == foundB, $foundA & " == " & $foundB & " distA: " & $distA & " distB: " & $distB)
-      sum += visited
-
-    echo "Million tree"
-    echo "visited ", sum ," nodes for ", test_runs,
-         " random findings (", sum / test_runs, " per lookup)\n"
-
-main()
-quit(0)
-
-import ../fancygl
 
 let (window, context) = defaultSetup()
 
 glPointSize(5)
 
-var tmp = newSeq[Vec4f](1000)
 
-for v in tmp.mitems():
+
+let tree = rand_tree(1000)
+
+let posBuffer = arrayBuffer(tree.data).view(pos)
+
+var colors = newSeq[Vec3f](1000)
+for v in colors.mitems():
   v.x = rand_f32()
   v.y = rand_f32()
   v.z = rand_f32()
-  v.w = 1.0f
 
-let colors   = arrayBuffer(tmp)
-
-for v in tmp.mitems():
-  v.x = rand_f32() * 2 - 1
-  v.y = rand_f32() * 2 - 1
-  v.z = rand_f32() * 2 - 1
-  v.w = 1.0f
-
-var vertices: ArrayBuffer[Vec4f] = arraybuffer(tmp)
+let colorsBuffer = arrayBuffer(colors)
 
 
 var evt: Event
@@ -229,13 +202,13 @@ var queryPos: Vec4f = vec4f(0,0,0,1)
 
 let cubeVertices = arrayBuffer([
   vec4f( 1, 1, 1, 1),
-  vec4f(-1, 1, 1, 1),
-  vec4f( 1,-1, 1, 1),
-  vec4f(-1,-1, 1, 1),
-  vec4f( 1, 1,-1, 1),
-  vec4f(-1, 1,-1, 1),
-  vec4f( 1,-1,-1, 1),
-  vec4f(-1,-1,-1, 1)
+  vec4f( 0, 1, 1, 1),
+  vec4f( 1, 0, 1, 1),
+  vec4f( 0, 0, 1, 1),
+  vec4f( 1, 1, 0, 1),
+  vec4f( 0, 1, 0, 1),
+  vec4f( 1, 0, 0, 1),
+  vec4f( 0, 0, 0, 1)
 ])
 
 let cubeLineIndices = elementArrayBuffer([
@@ -254,6 +227,38 @@ let cubeLineIndices = elementArrayBuffer([
 ])
 
 var linePositions = newArrayBuffer[Vec4f](1000)
+
+let boxesBuffer = newArrayBuffer[Box](1000)
+let boxesMinView = boxesBuffer.view(min)
+let boxesMaxView = boxesBuffer.view(max)
+
+proc drawBoxes(proj,modelView: Mat4f, maxDepth: int): void =
+  let boxes = tree.leafNodeBoxes(maxDepth)
+  boxesBuffer.setData(boxes)
+
+  shadingDsl:
+    primitiveMode = GL_LINES
+    numVertices = 24
+    indices = cubeLineIndices
+    numInstances = boxes.len
+    uniforms:
+      modelView
+      proj
+    attributes:
+      a_vertex = cubeVertices
+      a_max = boxesMaxView {.divisor: 1.}
+      a_min = boxesMinView {.divisor: 1.}
+    vertexMain:
+      """
+      vec4 pos_ws = vec4(mix(a_min + 0.001, a_max - 0.001, a_vertex.xyz), 1);
+      gl_Position = proj * modelView * pos_ws;
+      """
+    fragmentMain:
+      """
+      color = vec4(1);
+      """
+
+
 
 proc lines(mvp: Mat4f; vertices: openarray[Vec4f]): void =
   linePositions.setData(vertices)
@@ -274,17 +279,30 @@ proc lines(mvp: Mat4f; vertices: openarray[Vec4f]): void =
       color = vec4(1);
       """
 
+
+var rotationX, rotationY: float32
+var maxDepth = 0
+
 while runGame:
 
   while pollEvent(evt):
     if evt.kind == QuitEvent:
       runGame = false
       break
-    if evt.kind == KeyDown and evt.key.keysym.scancode == SDL_SCANCODE_ESCAPE:
-      runGame = false
+    if evt.kind == KeyDown:
+      if evt.key.keysym.scancode == SDL_SCANCODE_ESCAPE:
+        runGame = false
+      if evt.key.keysym.scancode == SDL_SCANCODE_KP_PLUS:
+        maxDepth += 1
+      if evt.key.keysym.scancode == SDL_SCANCODE_KP_MINUS:
+        maxDepth -= 1
+
     if evt.kind == MouseMotion:
       queryPos.x =     (evt.motion.x / window.size.x) * 2 - 1
       queryPos.y = 1 - (evt.motion.y / window.size.y) * 2
+
+      rotationY += float32(evt.motion.xrel) * 0.001f
+      rotationX += float32(evt.motion.yrel) * 0.001f
 
   let time = timer.time.float32
 
@@ -294,7 +312,8 @@ while runGame:
     .inverse                     # the camera matrix needs to be inverted
 
   let modelMat = mat4f(1)
-    #.rotateY(time)               # rotate the triangle
+    .rotateY(rotationY)    # rotate the triangle
+    .rotateX(rotationX)
     .scale(3)                    # scale the triangle to be big enough on screen
 
   glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
@@ -302,30 +321,30 @@ while runGame:
   var selected = 0
   var selected_dist = float32(Inf)
 
-  for i, v in tmp:
-    let dist = length(v.xyz - queryPos.xyz)
+  for i, node in tree.data:
+    let dist = length(node.pos.xyz - queryPos.xyz)
     if dist < selected_dist:
       selected = i
       selected_dist = dist
 
   shadingDsl:
     primitiveMode = GL_POINTS
-    numVertices = tmp.len
+    numVertices = 1000
     uniforms:
       modelView = viewMat * modelMat
       proj
       select    = int32(selected)
     attributes:
-      a_vertex = vertices
-      a_color  = colors
+      a_vertex = posBuffer
+      a_color  = colorsBuffer
     vertexMain:
       """
-      gl_Position = proj * modelView * a_vertex;
+      gl_Position = proj * modelView * vec4(a_vertex, 1);
       //gl_Position = a_vertex;
       if (select == gl_VertexID) {
         v_color = vec4(1);
       } else {
-        v_color = a_color;
+        v_color = vec4(a_color, 1);
       }
       """
     vertexOut:
@@ -335,6 +354,7 @@ while runGame:
       color = v_color;
       """
 
+  #[
   shadingDsl:
     primitiveMode = GL_LINES
     numVertices = 24
@@ -353,23 +373,29 @@ while runGame:
       """
       color = vec4(1);
       """
+  ]#
+
+  drawBoxes(proj, viewMat * modelMat, maxDepth)
+
 
   let mvp = proj * viewMat * modelMat
 
-  var a,b,c,d, e, f: Vec4f = queryPos
+  var a,b,c,d,e: Vec4f = queryPos
   a.x = -1
   b.x =  1
   c.y = -1
   d.y =  1
-  e.xy = tmp[selected].xy
+  e.xy = tree.data[selected].pos.xy
 
+  #[
   lines(mvp, [
     vec4f( 1, 1, queryPos.z, 1), vec4f(-1, 1, queryPos.z, 1),
     vec4f(-1, 1, queryPos.z, 1), vec4f(-1,-1, queryPos.z, 1),
     vec4f(-1,-1, queryPos.z, 1), vec4f( 1,-1, queryPos.z, 1),
     vec4f( 1,-1, queryPos.z, 1), vec4f( 1, 1, queryPos.z, 1),
-    a,b,c,d, queryPos, e, e, tmp[selected], tmp[selected], queryPos
+    a,b,c,d, queryPos, e, e, vec4f(tree.data[selected].pos, 1), vec4f(tree.data[selected].pos, 1), queryPos
   ])
+  ]#
 
   glSwapWindow(window)
 
