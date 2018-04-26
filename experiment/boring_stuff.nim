49bdfeab15f874e903ec52fb7c76eba418e31d3c
Arne Döring <arne.doering@gmx.net>
2018-04-26 18:28:03 +0200

    renaming and added forgotten stuff

---
+++ experiment/boring_stuff.nim
@@ -0,0 +1,114 @@
+proc makeUniqueData[T](arg: var openarray[T]): int =
+  ## removes consecutive duplicate elements from `arg`. Since this
+  ## operates on an `openarray` elements are not really removed, but
+  ## rearranged, so that all the non-unique elements are at the end,
+  ## and the the elements in the range `0 ..< result` will be
+  ## unique. This functions expects ordered input. `result` will
+  ## contain the amount of unique elements in arg.
+
+  if arg.len <= 1:
+    # sequentces with one or less elements can never have duplicates
+    return
+
+  var i,j : int
+  while i < arg.len:
+    template a: var T = arg[i]
+    template b: var T = arg[j]
+    if arg[j] != arg[i]:
+      # found an element that is different than the previeous element
+      j += 1
+      # this increment before the swap is correct, because arg[0]
+      # should not be changed. Neither should the last written element
+      # be overwritten.
+      swap(arg[i], arg[j])
+
+  # plus 1 because j is the index of the last elemnt, not the length
+  j + 1
+
+proc makeUnique[T](arg: var seq[T]): void =
+  ## removes consecutive duplicate elements from `arg`. This works
+  ## like `makeUniqueData`, but it changes the length of `arg`
+  ## the amount of unique elements.
+  arg.setLen(arg.makeUniqueData)
+
+
+proc sortAndUnique[T](arg: var seq[T]): void =
+  arg.sort(cmp)
+  arg.makeUnique
+
+## NimNode utils
+
+iterator arguments*(n: NimNode): NimNode {.inline.} =
+  ## Iterates over the arguments of a call ``n``.
+  for i in 1 ..< n.len:
+    yield n[i]
+
+iterator depthFirstTraversal*(n: NimNode): NimNode =
+  var stack = newSeq[tuple[n: NimNode,i: int]](0)
+  stack.add((n: n, i: 0))
+  yield stack[^1].n
+  while stack.len > 0:
+    template i: untyped = stack[^1].i
+    template n: untyped = stack[^1].n
+    while i < n.len:
+      let child = n[i]
+      i += 1
+      stack.add((n: child, i: 0))
+      yield stack[^1].n
+    discard stack.pop
+
+iterator fields*(typeAst: NimNode): tuple[memberSym, typeSym: NimNode] =
+  let parent: NimNode =
+    if typeAst.kind == nnkObjectTy:
+      typeAst[2]
+    else:
+      echo typeAst.treeRepr
+      typeAst
+
+  for identDefs in parent:
+    let typeSym = identDefs[^2]
+    for i in 0 ..< identDefs.len-2:
+      let memberSym = identDefs[i]
+      yield((memberSym: memberSym, typeSym: typeSym))
+
+
+## gl wrapper ##
+
+type
+  Texture1D            = object
+    handle: uint32
+  Texture2D            = object
+    handle: uint32
+  Texture3D            = object
+    handle: uint32
+  TextureCube          = object
+    handle: uint32
+  Texture2DShadow      = object
+    handle: uint32
+  TextureCubeShadow    = object
+    handle: uint32
+  Texture2DArray       = object
+    handle: uint32
+  Texture2DArrayShadow = object
+    handle: uint32
+
+proc texture*(sampler: Texture2D;            P: Vec2f; bias: float32 = 0): Vec4f =
+  quit("only implemented in shader")
+
+proc texture*(sampler: Texture3D;            P: Vec3f; bias: float32 = 0): Vec4f =
+  quit("only implemented in shader")
+
+proc texture*(sampler: TextureCube;          P: Vec3f; bias: float32 = 0): Vec4f =
+  quit("only implemented in shader")
+
+proc texture*(sampler: Texture2DShadow;      P: Vec3f; bias: float32 = 0): Vec4f =
+  quit("only implemented in shader")
+
+proc texture*(sampler: TextureCubeShadow;    P: Vec4f; bias: float32 = 0): Vec4f =
+  quit("only implemented in shader")
+
+proc texture*(sampler: Texture2DArray;       P: Vec3f; bias: float32 = 0): Vec4f =
+  quit("only implemented in shader")
+
+proc texture*(sampler: Texture2DArrayShadow; P: Vec4f): Vec4f =
+  quit("only implemented in shader")
---
+++ experiment/glslTranslate.nim
@@ -0,0 +1,112 @@
+## this file is for all functions that are related to translating Nim
+## directly to glsl.
+
+import normalizeType, glm, ast_pattern_matching
+
+
+proc expectIntIn(arg: NimNode; slice: Slice[int]): void =
+  if arg.intVal notin slice:
+    error("expect integer literal in range: " & $slice.a & " .. " & $slice.b & " but got " & $arg.intVal, arg)
+
+proc glslType*(arg: NimNode): string {.compileTime.} =
+  let arg = arg.normalizeType
+
+
+  arg.matchAst:
+  of ident"float32":
+    return "float"
+  of ident"float64":
+    return "double"
+  of ident"int32":
+    return "int"
+  of ident"bool":
+    return "bool"
+  of ident"Texture1D":
+    return "sampler1D"
+  of ident"Texture2D":
+    return "sampler2D"
+  of ident"Texture3D":
+    return "sampler3D"
+  of ident"TextureCube":
+    return "samplerCube"
+  of ident"Texture2DShadow":
+    return "sampler2DShadow"
+  of ident"TextureCubeShadow":
+    return "samplerCubeShadow"
+  of ident"Texture2DArray":
+    return "sampler2DArray"
+  of ident"Texture2DArrayShadow":
+    return "sampler2DArrayShadow"
+
+  of nnkBracketExpr(ident"Vec", `sizeLit`, `Tsym`):
+
+    Tsym.matchAst:
+    of ident"float32":
+      result = "vec"
+    of ident"float64":
+      result = "dvec"
+    of ident"int32":
+      result = "ivec"
+    of ident"bool":
+      result = "bvec"
+
+    sizeLit.expectIntIn 2..4
+    result.add sizeLit.intVal
+
+  of nnkBracketExpr(ident"Mat", `sizeLit1`, `sizeLit2`, `Tsym`):
+
+    Tsym.matchAst:
+    of ident"float32":
+      result = "mat"
+    of ident"float64":
+      result = "dmat"
+    of ident"int32":
+      result = "imat"
+    of ident"bool":
+      result = "bmat"
+
+    sizeLit1.expectIntIn 2..4
+    sizeLit2.expectIntIn 2..4
+
+    let intVal1 = sizeLit1.intVal
+    let intVal2 = sizeLit2.intVal
+
+    result.add intVal1
+    if intVal2 != intVal1:
+      result.add "x"
+      result.add intVal2
+
+
+when isMainModule:
+  macro testGlslType(arg: typed): untyped =
+    var expected: string
+    for varSection in arg:
+      if varSection.kind == nnkVarSection:
+        let identDefs = varSection[0]
+        let sym = identDefs[0]
+
+        let glslType = sym.getTypeInst.glslType
+        echo glslType
+        assert glslType == expected
+      else:
+        expected = varSection.strVal
+
+  testGlslType:
+    ## vec4
+    var a: Vec4f
+    ## vec4
+    var b: Vec4[float32]
+    ## vec4
+    var c: Vec[4,float32]
+    ## mat4
+    var d: Mat4f
+    ## mat4
+    var e: Mat4[float32]
+    ## mat4
+    var f: Mat[4,4,float32]
+    ## float
+    var g: float32
+    ## mat3x4
+    var h: Mat[3,4, float32]
+    ## mat4x2
+    var i: Mat[4,2, float32]
