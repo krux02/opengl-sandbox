fae022bf399efbdffb70cc90dd5b0a2eee5a571b
Arne Döring <arne.doering@gmx.net>
2018-06-06 12:12:57 +0200

    nicer test case

--- experiment/std140AlignedWrite.nim
+++ experiment/std140AlignedWrite.nim
@@ -34,43 +34,45 @@ proc getAlignment(value : tuple | object): int32 =
   for x in value.fields:
     result = max(result, x.getAlignment)
 
-template debug(args: varargs[untyped]): untyped =
+var offsets = newSeq[int32](0)
+
+template debug(result: tuple[offset, align: int32]): untyped =
   when isMainModule:
-    echo args
+    offsets.add result.offset
 
 proc std140AlignedWrite*(dst: pointer, offset: int32, value: bool): tuple[offset, align: int32] =
   result.align = 4
   result.offset = align(offset, 4)
 
-  debug "offset: ", result.offset
+  debug result
   dst.write(result.offset, value)
 
 proc std140AlignedWrite*(dst: pointer, offset: int32, value: SomeNumber): tuple[offset, align: int32] =
   result.align = max(sizeof(SomeNumber), 4)
   result.offset = align(offset, result.align)
 
-  debug "offset: ", result.offset
+  debug result
   dst.write(result.offset, value)
 
 proc std140AlignedWrite*[T](dst: pointer, offset: int32, value: Vec4[T]): tuple[offset, align: int32] =
   result.align = sizeof(T) * 4
   result.offset = align(offset, result.align)
 
-  debug "offset: ", result.offset
+  debug result
   dst.write(result.offset, value)
 
 proc std140AlignedWrite*[T](dst: pointer, offset: int32, value: Vec3[T]): tuple[offset, align: int32] =
   result.align = sizeof(T) * 4
   result.offset = align(offset, result.align)
 
-  debug "offset: ", result.offset
+  debug result
   dst.write(result.offset, value)
 
 proc std140AlignedWrite*[T](dst: pointer, offset: int32, value: Vec2[T]): tuple[offset, align: int32] =
   result.align = sizeof(T) * 2
   result.offset = align(offset, result.align)
 
-  debug "offset: ", result.offset
+  debug result
   dst.write(result.offset, value)
 
 proc std140AlignedWrite*[N,M,T](dst: pointer, offset: int32, value: Mat[N,M,T]): tuple[offset, align: int32] =
@@ -78,7 +80,7 @@ proc std140AlignedWrite*[N,M,T](dst: pointer, offset: int32, value: Mat[N,M,T]):
   result.align = getAlignment(value)
   result.offset = align(offset, result.align)
 
-  debug "offset: ", result.offset
+  debug result
   for v in value.arr:
     dst.write(result.offset, v)
     result.offset = align(result.offset, result.align)
@@ -99,55 +101,22 @@ proc std140AlignedWrite*(dst: pointer, offset: int32, value: tuple | object): tu
   for x in value.fields:
     tmp = std140AlignedWrite(dst, tmp.offset, x)
 
-
   result.offset = align(tmp.offset, result.align)
 
-
 when isMainModule:
+  template foo(value: untyped, expectedOffsets: untyped): untyped =
+    offsets.setLen 0
+    discard std140AlignedWrite(data, 0, value)
+    if offsets != expectedOffsets:
+      echo "offsets != expectedOffsets"
+      echo offsets
+      echo expectedOffsets
 
   type
     Light = object
       position_ws : Vec4f
       color : Vec4f
 
-  var buffer = newSeq[byte](1024)
-  var data = buffer[0].addr
-
-  var b00: tuple[
-    b0a: int32,
-    b0c: int64,
-  ]
-
-  var b01: tuple[
-    b1a: int32,
-    b1b: int32,
-    b1c: int64,
-  ]
-
-  var b02: tuple[
-    b2a: array[2,int32],
-    b2c: int64,
-  ]
-
-  var b03: tuple[
-    b3a: bool,
-    b3b: bool,
-    b3c: bool,
-    b3d: float32,
-  ]
-
-  var b04: tuple[
-    b4a: Mat2f,
-    b4b: Mat2f,
-    b4c: Mat4f,
-  ]
-
-  var b05: tuple[
-    b5a: array[2,Mat2f],
-    b5c: Mat4f,
-  ]
-
-  type
     MyStruct = object
       f1: float32
       f2: float32
@@ -161,80 +130,120 @@ when isMainModule:
       m0: Vec4d
       f1: float32
 
+  var buffer = newSeq[byte](1024)
+  var data = buffer[0].addr
+
+  var b00: tuple[
+    b0a: int32,
+    b0c: int64,
+  ]
+  foo b00, @[0'i32, 8]
+
+  var b01: tuple[
+    b1a: int32,
+    b1b: int32,
+    b1c: int64,
+  ]
+  foo b01, @[0'i32, 4, 8]
+
+
+  var b02: tuple[
+    b2a: array[2,int32],
+    b2c: int64,
+  ]
+  foo b02, @[0'i32, 16, 32]
+
+  var b03: tuple[
+    b3a: bool,
+    b3b: bool,
+    b3c: bool,
+    b3d: float32,
+  ]
+  foo b03, @[0'i32, 4, 8, 12]
+
+  var b04: tuple[
+    b4a: Mat2f,
+    b4b: Mat2f,
+    b4c: Mat4f,
+  ]
+  foo b04, @[0'i32, 32, 64]
+
+  var b05: tuple[
+    b5a: array[2,Mat2f],
+    b5c: Mat4f,
+  ]
+  foo b05, @[0'i32, 32, 64]
+
   var b06: tuple[
     b6a: MyStruct,
     b6b: MyStruct,
   ]
+  foo b06, @[0'i32, 4, 16, 20]
 
   var b07: tuple[
     b7a: Vec3f,
     b7b: float32
   ]
+  foo b07, @[0'i32, 12]
 
   var b08: tuple[
     b8a: Vec3f,
     b8b: float64,
   ]
+  foo b08, @[0'i32, 16]
 
   var b09: tuple[
     b9a: Vec3d,
     b9b: Vec2f,
   ]
+  foo b09, @[0'i32, 24]
 
   var b10: tuple[
     b10a: bool,
     b10b: bool,
   ]
+  foo b10, @[0'i32, 4]
 
   var b11: tuple[
     b11a: StructA,
     b11b: float32,
   ]
+  foo b11, @[0'i32, 16, 32, 48]
 
   var b12: tuple[
     b12a: StructB,
     b12b: float32,
   ]
-
+  foo b12, @[0'i32, 32, 64]
 
   var b13: tuple[
     b13a: float32,
     b13b: StructB,
   ]
+  foo b13, @[0'i32, 32, 64]
 
-  template foo(str: string, value: untyped): untyped =
-    echo str
-    discard std140AlignedWrite(data, 0, value)
+  var value1: tuple[
+    a: Vec3f;
+    b: Vec4f;
+    c: float32
+  ]
+  foo value1, @[0'i32, 16, 32]
 
+  var value2: tuple[
+    time: float32,
+    M, V, P: Mat4f,
+    lights: array[4,Light]
+  ]
+  foo value2, @[0'i32, 16, 80, 144, 208, 224, 240, 256, 272, 288, 304, 320]
 
-  foo "b00", b00
-  foo "b01", b01
-  foo "b02", b02
-  foo "b03", b03
-  foo "b04", b04
-  foo "b05", b05
-  foo "b06", b06
-  foo "b07", b07
-  foo "b08", b08
-  foo "b09", b09
-  foo "b10", b10
-  foo "b11", b11
-  foo "b12", b12
-  foo "b13", b13
+  var value3: tuple[
+    m: Mat3f,
+    f: float32
+  ]
+  foo value3, @[0'i32, 48]
 
-
-  echo "tuple 1"
-  var value1: tuple[a: Vec3f; b: Vec4f; c: float32]
-  discard std140AlignedWrite(data, 0, value1)
-
-  echo "tuple 2"
-  var value2: tuple[time: float32, M, V, P: Mat4f, lights: array[4,Light]]
-  discard std140AlignedWrite(data, 0, value2)
-
-  echo "tuple 3"
-  var value3: tuple[m: Mat3f, f: float32]
-  discard std140AlignedWrite(data, 0, value3)
-
-  echo "tuple 4"
-  var value4: tuple[b: bool; m: Mat4f]
-  discard std140AlignedWrite(data, 0, value4)
+  var value4: tuple[
+    b: bool;
+    m: Mat4f
+  ]
+  foo value4, @[0'i32, 16]
