30e95e2444100de19b1369463e5a278e3e22282c
Arne Döring <arne.doering@gmx.net>
2026-02-15 11:48:58 +0100

    active hello shapes in runall

--- examples/hello_shapes.nim
+++ examples/hello_shapes.nim
@@ -2,11 +2,6 @@ import ../fancygl
 
 import sequtils
 
-let (window, context) = defaultSetup()
-let windowsize = window.size
-
-let projection_mat : Mat4f = perspective(45'f32, window.aspectRatio, 0.1, 100.0)
-
 type
   IdMesh = enum
     IdCone,
@@ -28,7 +23,7 @@ type
 
 var meshes: array[IdMesh, SimpleMesh]
 
-block init:
+proc init(): void =
   const numSegments = 32
 
   var verticesSeq = newSeq[Vec4f](0)
@@ -120,182 +115,194 @@ block init:
   colors = arrayBuffer(colorsSeq)
   indices = elementArrayBuffer(indicesSeq)
 
-var planeVertices = arrayBuffer([
-  vec4f(0,0,0,1), vec4f( 1, 0,0,0), vec4f( 0, 1,0,0),
-  vec4f(0,0,0,1), vec4f( 0, 1,0,0), vec4f(-1, 0,0,0),
-  vec4f(0,0,0,1), vec4f(-1, 0,0,0), vec4f( 0,-1,0,0),
-  vec4f(0,0,0,1), vec4f( 0,-1,0,0), vec4f( 1, 0,0,0)
-])
+proc main*(window: Window): void =
+  init()
 
-# for each mesh create one node in the world to Draw it there
-var worldNodes : array[IdMesh, WorldNode] = [
-  newWorldNode(-3, 3, 1),
-  newWorldNode( 3,-3, 1),
-  newWorldNode(-3,-3, 1),
-  newWorldNode( 3, 3, 1),
-  newWorldNode( 0, 0, 1),
-  newWorldNode( 0,-6, 1),
-  newWorldNode(-6, 0, 1)
-]
+  let windowsize = window.size
 
-var camera = newWorldNode(0,9,4)
-camera.lookAt(vec3f(0.1,0.2,1))
+  glViewport(0,0,windowsize.x, windowsize.y)
+  let projection_mat : Mat4f = perspective(45'f32, window.aspectRatio, 0.1, 100.0)
 
-var planeNode = newWorldNode()
-var runGame: bool = true
-var frame = 0
-var noiseArray: array[21, float32]
+  var planeVertices = arrayBuffer([
+    vec4f(0,0,0,1), vec4f( 1, 0,0,0), vec4f( 0, 1,0,0),
+    vec4f(0,0,0,1), vec4f( 0, 1,0,0), vec4f(-1, 0,0,0),
+    vec4f(0,0,0,1), vec4f(-1, 0,0,0), vec4f( 0,-1,0,0),
+    vec4f(0,0,0,1), vec4f( 0,-1,0,0), vec4f( 1, 0,0,0)
+  ])
 
-for x in noiseArray.mitems:
-  x = (rand_f32()*2-1) * 0.01f;
+  # for each mesh create one node in the world to Draw it there
+  var worldNodes : array[IdMesh, WorldNode] = [
+    newWorldNode(-3, 3, 1),
+    newWorldNode( 3,-3, 1),
+    newWorldNode(-3,-3, 1),
+    newWorldNode( 3, 3, 1),
+    newWorldNode( 0, 0, 1),
+    newWorldNode( 0,-6, 1),
+    newWorldNode(-6, 0, 1)
+  ]
 
-while runGame:
-  frame += 1
+  var camera = newWorldNode(0,9,4)
+  camera.lookAt(vec3f(0.1,0.2,1))
 
-  # just some meaningless numbers to make the shapes rotate
-  worldNodes[IdCone].turnRelativeZ(noiseArray[0])
-  worldNodes[IdCone].turnRelativeX(noiseArray[1])
-  worldNodes[IdCone].turnRelativeY(noiseArray[2])
+  var planeNode = newWorldNode()
+  var runGame: bool = true
+  var frame = 0
+  var noiseArray: array[21, float32]
 
-  worldNodes[Idcylinder].turnRelativeX(noiseArray[3])
-  worldNodes[Idcylinder].turnRelativeY(noiseArray[4])
-  worldNodes[Idcylinder].turnRelativeZ(noiseArray[5])
+  for x in noiseArray.mitems:
+    x = (rand_f32()*2-1) * 0.01f;
 
-  worldNodes[Idicosphere].turnRelativeX(noiseArray[6])
-  worldNodes[Idicosphere].turnRelativeY(noiseArray[7])
-  worldNodes[Idicosphere].turnRelativeZ(noiseArray[8])
+  while runGame:
+    frame += 1
 
-  worldNodes[Idsphere].turnRelativeX(noiseArray[9])
-  worldNodes[Idsphere].turnRelativeY(noiseArray[10])
-  worldNodes[Idsphere].turnRelativeZ(noiseArray[11])
+    # just some meaningless numbers to make the shapes rotate
+    worldNodes[IdCone].turnRelativeZ(noiseArray[0])
+    worldNodes[IdCone].turnRelativeX(noiseArray[1])
+    worldNodes[IdCone].turnRelativeY(noiseArray[2])
 
-  worldNodes[Idbox].turnRelativeX(noiseArray[12])
-  worldNodes[Idbox].turnRelativeY(noiseArray[13])
-  worldNodes[Idbox].turnRelativeZ(noiseArray[14])
+    worldNodes[Idcylinder].turnRelativeX(noiseArray[3])
+    worldNodes[Idcylinder].turnRelativeY(noiseArray[4])
+    worldNodes[Idcylinder].turnRelativeZ(noiseArray[5])
 
-  worldNodes[Idtetraeder].turnRelativeX(noiseArray[15])
-  worldNodes[Idtetraeder].turnRelativeY(noiseArray[16])
-  worldNodes[Idtetraeder].turnRelativeZ(noiseArray[17])
+    worldNodes[Idicosphere].turnRelativeX(noiseArray[6])
+    worldNodes[Idicosphere].turnRelativeY(noiseArray[7])
+    worldNodes[Idicosphere].turnRelativeZ(noiseArray[8])
 
-  worldNodes[Idtorus].turnRelativeX(noiseArray[18])
-  worldNodes[Idtorus].turnRelativeY(noiseArray[19])
-  worldNodes[Idtorus].turnRelativeZ(noiseArray[20])
+    worldNodes[Idsphere].turnRelativeX(noiseArray[9])
+    worldNodes[Idsphere].turnRelativeY(noiseArray[10])
+    worldNodes[Idsphere].turnRelativeZ(noiseArray[11])
 
-  # the plane on the ground is rotating the camera is still.  It
-  # really provides the illusion the camera would rotate around the
-  # shapes though
-  planeNode.turnAbsoluteZ(0.0001)
+    worldNodes[Idbox].turnRelativeX(noiseArray[12])
+    worldNodes[Idbox].turnRelativeY(noiseArray[13])
+    worldNodes[Idbox].turnRelativeZ(noiseArray[14])
 
-  for evt in events():
-    if evt.kind == QUIT:
-      runGame = false
-      break
-    if evt.kind == KEY_DOWN:
-      case evt.key.keysym.scancode
-      of SCANCODE_ESCAPE:
+    worldNodes[Idtetraeder].turnRelativeX(noiseArray[15])
+    worldNodes[Idtetraeder].turnRelativeY(noiseArray[16])
+    worldNodes[Idtetraeder].turnRelativeZ(noiseArray[17])
+
+    worldNodes[Idtorus].turnRelativeX(noiseArray[18])
+    worldNodes[Idtorus].turnRelativeY(noiseArray[19])
+    worldNodes[Idtorus].turnRelativeZ(noiseArray[20])
+
+    # the plane on the ground is rotating the camera is still.  It
+    # really provides the illusion the camera would rotate around the
+    # shapes though
+    planeNode.turnAbsoluteZ(0.0001)
+
+    for evt in events():
+      if evt.kind == QUIT:
         runGame = false
         break
+      if evt.kind == KEY_DOWN:
+        case evt.key.keysym.scancode
+        of SCANCODE_ESCAPE:
+          runGame = false
+          break
 
-      of SCANCODE_F10:
-        window.screenshot
+        of SCANCODE_F10:
+          window.screenshot
 
-      else:
-        discard
+        else:
+          discard
 
-  glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
 
-  for i, node in worldNodes:
-    let mesh = meshes[i]
+    for i, node in worldNodes:
+      let mesh = meshes[i]
+
+      shadingDsl:
+        primitiveMode = GL_TRIANGLES
+        numVertices = mesh.numVertices
+        vertexOffset = mesh.baseIndex
+        baseVertex = mesh.baseVertex
+        indices = indices
+
+        uniforms:
+          proj = projection_mat
+          modelView = camera.viewMat * node.modelMat
+
+        attributes:
+          a_vertex = vertices
+          a_normal = normals
+          a_color  = colors
+
+        vertexMain:
+          """
+          gl_Position = proj * modelView * a_vertex;
+          v_vertex = a_vertex;
+          v_normal = modelView * a_normal;
+          v_color = a_color;
+          """
+        vertexOut:
+          "out vec4 v_vertex"
+          "out vec4 v_normal"
+          "out vec4 v_color"
+
+        fragmentMain:
+          """
+          // cheap fake lighting from camera direction
+          color = v_color * v_normal.z;
+          """
+
+    let modelViewProj = projection_mat * camera.viewMat * planeNode.modelMat
+
+    # shapes with infinitely far away points, can't interpolate alon the vertices,
+    # therefore so varyings don't work.
+    # The matrix transformation of can be inverted in the fragment shader, so that that in this case
+    # object space coordinates can be recontructed.
 
     shadingDsl:
       primitiveMode = GL_TRIANGLES
-      numVertices = mesh.numVertices
-      vertexOffset = mesh.baseIndex
-      baseVertex = mesh.baseVertex
-      indices = indices
-
+      numVertices = planeVertices.len
       uniforms:
-        proj = projection_mat
-        modelView = camera.viewMat * node.modelMat
+        modelViewProj
+        invModelViewProj = inverse(modelViewProj)
+        invWindowSize    = vec2f(1 / float32(windowSize.x), 1 / float32(windowSize.y))
 
       attributes:
-        a_vertex = vertices
-        a_normal = normals
-        a_color  = colors
+        a_vertex   = planeVertices
 
       vertexMain:
         """
-        gl_Position = proj * modelView * a_vertex;
-        v_vertex = a_vertex;
-        v_normal = modelView * a_normal;
-        v_color = a_color;
+        gl_Position = modelViewProj * a_vertex;
         """
-      vertexOut:
-        "out vec4 v_vertex"
-        "out vec4 v_normal"
-        "out vec4 v_color"
 
       fragmentMain:
         """
-        // cheap fake lighting from camera direction
-        color = v_color * v_normal.z;
+        color = vec4(1,0,1,0);
+        vec4 tmp = gl_FragCoord;
+
+        // reconstructing normalized device coordinates from fragment depth, fragment position.
+        vec4 ndc_pos;
+        ndc_pos.xy = gl_FragCoord.xy * invWindowSize * 2 - 1;
+        ndc_pos.z  = gl_FragCoord.z                  * 2 - 1;
+        ndc_pos.w = 1;
+
+        // coordinate in object space coordinate
+        vec4 objPos = invModelViewProj * ndc_pos;
+        // the projection part of this operation alternates the w component of the vector
+        // in order to make xyz components meaningful, a normalization is required
+        objPos /= objPos.w;
+
+        // objPos.z is expected to be 0, fract on an almost 0 value would lead to weird patterns
+        // an optimization would be to shrinkthe matrices, so that it isn't calculated anymore.
+        vec2 texcoord = objPos.xy;
+        vec2 texcoord_dx = fwidth(texcoord);
+        //vec2 texcoord_dy = dFdy(texcoord);
+        color = vec4(0,0,0,1);
+        // antialiasing
+        for(int i = 0; i <= 4; ++i) {
+          vec2 offset = texcoord_dx * 0.5 * (float(i-2) / 2.0);
+          color.rg += fract(texcoord + offset) / 5;
+        }
+
         """
 
-  let modelViewProj = projection_mat * camera.viewMat * planeNode.modelMat
+    glSwapWindow(window)
 
-  # shapes with infinitely far away points, can't interpolate alon the vertices,
-  # therefore so varyings don't work.
-  # The matrix transformation of can be inverted in the fragment shader, so that that in this case
-  # object space coordinates can be recontructed.
-
-  shadingDsl:
-    primitiveMode = GL_TRIANGLES
-    numVertices = planeVertices.len
-    uniforms:
-      modelViewProj
-      invModelViewProj = inverse(modelViewProj)
-      invWindowSize    = vec2f(1 / float32(windowSize.x), 1 / float32(windowSize.y))
-
-    attributes:
-      a_vertex   = planeVertices
-
-    vertexMain:
-      """
-      gl_Position = modelViewProj * a_vertex;
-      """
-
-    fragmentMain:
-      """
-      color = vec4(1,0,1,0);
-      vec4 tmp = gl_FragCoord;
-
-      // reconstructing normalized device coordinates from fragment depth, fragment position.
-      vec4 ndc_pos;
-      ndc_pos.xy = gl_FragCoord.xy * invWindowSize * 2 - 1;
-      ndc_pos.z  = gl_FragCoord.z                  * 2 - 1;
-      ndc_pos.w = 1;
-
-      // coordinate in object space coordinate
-      vec4 objPos = invModelViewProj * ndc_pos;
-      // the projection part of this operation alternates the w component of the vector
-      // in order to make xyz components meaningful, a normalization is required
-      objPos /= objPos.w;
-
-      // objPos.z is expected to be 0, fract on an almost 0 value would lead to weird patterns
-      // an optimization would be to shrinkthe matrices, so that it isn't calculated anymore.
-      vec2 texcoord = objPos.xy;
-      vec2 texcoord_dx = fwidth(texcoord);
-      //vec2 texcoord_dy = dFdy(texcoord);
-      color = vec4(0,0,0,1);
-      // antialiasing
-      for(int i = 0; i <= 4; ++i) {
-        vec2 offset = texcoord_dx * 0.5 * (float(i-2) / 2.0);
-        color.rg += fract(texcoord + offset) / 5;
-      }
-
-      """
-
-  glSwapWindow(window)
+when isMainModule:
+  let (window, context) = defaultSetup()
+  main(window)
 
 # Local Variables:
 # compile-command: "cd examples; nim c -r hello_shapes.nim"
